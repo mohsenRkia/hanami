044145bc18d70f738f7972793886bbf7c01b8ad6
Mohsen <33616108+mohsenRkia@users.noreply.github.com>
2023-06-07 15:31:10 +0330

    Some Changes On Authentication Structure

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            refresh: false,
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
         }),
         vue({
             template: {
@@ -17,4 +20,9 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
