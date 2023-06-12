24c6b8e6090010979a66ffab9fcb0e431d5b39f3
majidch-1991 <132746256+majidch-1991@users.noreply.github.com>
2023-06-12 15:17:27 +0330

    Create Article Modules Migrations

---
+++ Modules/Article/Database/Migrations/2023_06_12_113535_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedInteger('category_id');
+            $table->boolean('status')->default(0);
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
