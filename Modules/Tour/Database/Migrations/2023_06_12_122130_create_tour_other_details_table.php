ad1501e39cedeb8d4f1212b606045465270a48f1
majidch-1991 <132746256+majidch-1991@users.noreply.github.com>
2023-06-12 16:07:17 +0330

    Create Tour Modules And Migrations

---
+++ Modules/Tour/Database/Migrations/2023_06_12_122130_create_tour_other_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tour_other_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('article_id');
+            $table->string('title')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tour_other_details');
+    }
+};
---
+++ Modules/Tour/Database/Migrations/2023_06_12_122159_create_type_movings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('type_movings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_en')->nullable();
+            $table->string('name_fa')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('type_movings');
+    }
+};
