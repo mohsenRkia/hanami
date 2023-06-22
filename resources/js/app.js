2e87176d329822e41c5869b9e5192ab163281f09
Mohsen <33616108+mohsenRkia@users.noreply.github.com>
2023-06-22 11:32:31 +0330

    Edit Some Changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,32 +9,29 @@ import { createApp } from 'vue';
 
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
-<<<<<<< HEAD
  * registering components with the application instance so they are ready
  * to use in your application's views. An example is included for you.
  */
-=======
- * registering components with the application instance 'so' they are ready
+
+ /* registering components with the application instance 'so' they are ready
  * to use in your application's views. An example is included for you.
  */
 import VueSweetalert2 from "vue-sweetalert2";
 // import 'sweetalert2/dist/sweetalert2.min.css'
 import '@sweetalert2/themes/borderless/borderless.scss';
->>>>>>> majid
+
 
 const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
 
-<<<<<<< HEAD
-=======
 
 //ADMIN
 import DeleteComponent from "@/Components/panel/DeleteComponent.vue";
 
 app.component('delete-component', DeleteComponent);
->>>>>>> majid
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -48,22 +45,16 @@ app.component('delete-component', DeleteComponent);
 // });
 
 /**
-<<<<<<< HEAD
  * Finally, we will attach the application instance to a HTML element with
-=======
  * Finally, we will attach the application instance to 'a' HTML element with
->>>>>>> majid
  * an "id" attribute of "app". This element is included with the "auth"
  * scaffolding. Otherwise, you will need to add an element yourself.
  */
 
-<<<<<<< HEAD
-=======
 const options = {
     confirmButtonColor: '#41b882',
     cancelButtonColor: '#ff7674',
 };
 
 app.use(VueSweetalert2,options);
->>>>>>> majid
 app.mount('#app');
